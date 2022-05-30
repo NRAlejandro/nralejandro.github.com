580338d23060cb3b9c888544299b40e586dfc36b
NRAlejandro <nicolas.ramirez.alejandro@gmail.com>
2022-05-30 15:57:00 -0300

    Deleted un-used link and info on data object

--- nraportfolio/src/components/DynamicSection/Data.js
+++ nraportfolio/src/components/DynamicSection/Data.js
@@ -4,11 +4,11 @@ export const AboutMeObj = {
   lightTextDesc: true,
   topLine: "Who I am and what I want",
   description:
-    "Im a Jr developer searching for a trainee or Jr position that allow me to develop myself as a programmer. As a self-learning dev I've become use to learn new skills on my own and be upfront with the regular basis frustration that come with that. My curiosity drives me to learn more and more thats why my goal is to start as a Front-End Developer and latter on my carreer switch to one of this three fields where my current interest resides on: Mobile Developer, Data Science or Game developer.",
+    "Im a Jr developer searching for a trainee or Jr position that allow me to develop myself as a programmer. As a self-learning dev I've become use to learn new skills on my own and be upfront with the regular basis frustration that comes with that. My curiosity drives me to learn more and more thats why my goal is to start as a Front-End Developer and latter on my carreer switch to one of these three fields where my current interest resides on: Mobile Developer, Data Science or Game developer.",
   buttonLabel: "Knowledge",
   imgStart: true,
   img: require("../../images/personal-goals.svg").default,
-  alt: " TO CHANGE ",
+  alt: "Personal goals",
   dark: true,
   primary: true,
   darkText: false,
